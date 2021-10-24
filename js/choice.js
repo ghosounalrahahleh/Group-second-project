1fbd2c15c089194f29dcccc73dbb6446012d4d17
Hassanwael <hassanwael1999@gmail.com>
2021-10-24 18:14:44 +0300

    Done

--- js/choice.js
+++ js/choice.js
@@ -1,3 +1,3 @@
-let name1=document.getElementById("user-N")
+let name1=document.getElementsByClassName('user-N')[0];
 let string3=sessionStorage.getItem('currentUser')
-console.log(string3.slice(0,string3.indexOf(" ")))
+name1.innerHTML=string3.slice(0,string3.indexOf(" "));
